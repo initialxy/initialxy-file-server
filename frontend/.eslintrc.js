db4a560ec9e384a2c509ad95bf9c3af9f9bacedb
Xingchen Yu <initialxy@gmail.com>
2021-12-28 15:33:42 -0800

    Fix version conflicts by down grading eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
     '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/no-empty-function': 'off'
+    '@typescript-eslint/no-empty-function': 'off',
+    'vue/multi-word-component-names': 'off'
   }
 }
